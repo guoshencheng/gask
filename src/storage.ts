19867d1417d8a6b83255f3dd972ccf780c6b3bd5
guozhencheng <648772021@qq.com>
2019-04-29 15:43:59 +0800

    refact storage to promise able for feature change

--- src/storage.ts
+++ src/storage.ts
@@ -8,7 +8,13 @@
  */
 import { homedir } from 'os';
 import { sep, parse } from 'path';
-import { statSync, mkdirSync, readFileSync, writeFileSync } from 'fs'
+import {
+  statSync,
+  mkdirSync,
+  readFileSync,
+  writeFileSync,
+  unlinkSync,
+} from 'fs'
 import { generate } from 'shortid'
 
 import Condition, { ConditionDescObj } from './condition'
@@ -42,32 +48,96 @@ const writeStorageFile = (file: string, data: any[]) => {
   writeFileSync(file, content, 'utf8')
 }
 
-export const create = (name: string, obj: any) => {
+export type StorageResult = {
+  success: boolean,
+  result?: any,
+  error?: any,
+}
+
+export const _drop = (name: string): Promise<StorageResult> => {
+  const file = storageFile(name)
+  checkAndFillPath(file)
+  return new Promise((resolve, reject) => {
+    try {
+      unlinkSync(file)
+      resolve({
+        success: true,
+      })
+    } catch (e) {
+      resolve({
+        success: false,
+        error: e,
+      })
+    }
+  })
+}
+
+export const create = async (name: string, obj: any): Promise<StorageResult> => {
   const file = storageFile(name)
   checkAndFillPath(file)
   const objs = readStorageFile(file) || []
-  writeStorageFile(file, objs.concat({
-    ...obj,
-    _id: generate(),
-  }))
+  return new Promise((resolve, reject) => {
+    try {
+      writeStorageFile(file, objs.concat({
+        ...obj,
+        _id: generate(),
+      }))
+      resolve({
+        success: true,
+      })
+    } catch (e) {
+      resolve({
+        success: false,
+        error: e
+      })
+    }
+  })
 }
 
-export const query = (name: string, condition: ConditionDescObj | Condition) => {
+export const query = (name: string, condition: ConditionDescObj | Condition): Promise<StorageResult> => {
   const file = storageFile(name)
   checkAndFillPath(file)
-  const contents = readStorageFile(file)
-  if (condition instanceof Condition) {
-    return contents.filter(condition.filter)
-  } else {
-    return contents.filter(new Condition(condition).filter)
-  }
+  return new Promise((resolve) => {
+    try {
+      const contents = readStorageFile(file)
+      let result
+      if (condition instanceof Condition) {
+        result = contents.filter(condition.filter)
+      } else {
+        result = contents.filter(new Condition(condition).filter)
+      }
+      resolve({
+        success: true,
+        result,
+      })
+    } catch (e) {
+      resolve({
+        success: false,
+        error: e,
+      })
+    }
+  })
 }
 
-export const update = (name: string, condition: ConditionDescObj | Condition, patch: any) => {
+export const update = (name: string, condition: ConditionDescObj | Condition, patch: any): Promise<StorageResult> => {
   const file = storageFile(name)
   checkAndFillPath(file)
-  const contents = readStorageFile(file)
-  const con = (condition instanceof Condition) ? condition : new Condition(condition)
-  const next = contents.map((item, index) => con.filter(item, index) ? ({ ...item, ...patch }) : item)
-  return next
+  return new Promise((resolve) => {
+    try {
+      const contents = readStorageFile(file)
+      const con = (condition instanceof Condition) ? condition : new Condition(condition)
+      const next = contents.map((item, index) => con.filter(item, index) ? ({ ...item, ...patch }) : item)
+      writeStorageFile(file, next)
+      resolve({
+        success: true,
+        result: next,
+      })
+    } catch (e) {
+      resolve({
+        success: false,
+        error: e,
+      })
+    }
+  })
 }
+
