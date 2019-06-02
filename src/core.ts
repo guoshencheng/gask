7d9ec7a013c75943aff41a0926b137ba75180b89
guozhencheng <648772021@qq.com>
2019-06-02 13:34:11 +0800

    add core task api

--- src/core.ts
+++ src/core.ts
@@ -1,7 +1,9 @@
 import {
   create as storageCreate,
+  update as storageUpdate,
   query as storageQuery,
 } from './storage'
+import { Op } from './condition'
 
 const name = 'task'
 
@@ -30,6 +32,56 @@ export const create = async (task: TaskObj): Promise<TaskObj> =>  {
   throw result.error
 }
 
+export const updateByIds = async (ids: string[], patch: any): Promise<TaskObj[]> => {
+  const result = await storageUpdate(name, {
+    id: {
+      [Op.in]: ids
+    }
+  }, patch)
+  if (result.success)  {
+    return result.result as TaskObj[]
+  }
+  throw result.error
+}
+
 export const check = async (ids: string[]): Promise<TaskObj[]> => {
-  // const tasks =  
+  return updateByIds(ids, {
+    checked: 1,
+  })
+}
+
+export const achive = async (ids: string[]): Promise<TaskObj[]> => {
+  return updateByIds(ids, {
+    achived: 1,
+  })
+}
+
+export type ListOptions = {
+  achived?: boolean,
+  checked?: boolean,
+}
+
+export const list = async (options: ListOptions): Promise<TaskObj[]> => {
+  const condition = {
+    achived: options.achived ? 1 : 0,
+    checked: options.checked ? 1 : 0,
+  }
+  const result = await storageQuery(name, condition)
+  if (result.success) {
+    return result.result
+  }
+  throw result.error
+}
+
+export type ListGroup = {
+  [key: string]: TaskObj[]
+}
+
+export const listByGroup = async (options: ListOptions): Promise<ListGroup> => {
+  const value = await list(options)
+  return value.reduce((pre, cur) => {
+    const groupName = cur.group
+    pre[groupName] = (pre[groupName] || []).concat(cur)
+    return pre
+  }, {} as ListGroup)
 }
